4b23b0b5ae86156b6b07501fc4e380c54e3dd379
אירינה קטלקין <ikatalkin@nitc.co.il>
2022-06-19 00:29:45 +0300

    Add functions_18062022

--- Linq_1.5_Take.Skip/Program.cs
+++ Linq_1.5_Take.Skip/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+List<string> countries = new List<string>() { "Israel", "USA", "Canada", "France", "German", "Ukrain", "India" };
+
+//IEnumerable<string> result = countries.Take(2);   // first 2 countries only
+IEnumerable<string> result = (from country in countries
+                             select country).Take(2);
+
+//IEnumerable<string> result = countries.Skip(2);   // All countries start from 3th 
+//IEnumerable<string> result = countries.TakeWhile(s => s.Length > 5);   // All countries then Length > 5
+//IEnumerable<string> result = countries.SkipWhile(s => s.Length > 5);
+
+foreach (var country in result)
+{
+    Console.WriteLine(country);
+}
