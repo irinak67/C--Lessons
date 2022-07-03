46fe665b1d33b5f3b98948b10cd201942aa6499a
אירינה קטלקין <ikatalkin@nitc.co.il>
2022-07-03 15:27:33 +0300

    Add lesson6.GC_IDisposable

---
+++ Lesson_6.GarbageCollection_disponse_destruction/MyClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace Lesson_6.GarbageCollection_disponse_destruction
+{
+    public class MyClass : IDisposable
+    {
+        public MyClass() { }
+        ~MyClass() { }     // destration
+
+        public void Dispose()
+        {
+           GC.Collect();
+        }
+    }
+
+    
+}
