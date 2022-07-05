554470d37f4112665d2468daec97a807b8451246
אירינה קטלקין <ikatalkin@nitc.co.il>
2022-07-05 13:18:49 +0300

    Add lesson8.Generics

---
+++ Lesson_8.Generic/Apple.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace Lesson_8.Generic
+{
+    public class Apple : Product<int>
+    {
+        public Apple(string name, int volume,int energy) : base(name, volume, energy)
+        {
+        }
+    }
+}
