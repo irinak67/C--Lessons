d0b15c506cb5f6f28bf667a724c6b1adab42ab91
אירינה קטלקין <ikatalkin@nitc.co.il>
2022-07-06 20:15:07 +0300

    Add to lesson0.IterationOperators

---
+++ Lesson_0.IterationOperators/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Lesson_0.IterationOperators;
+
+Exercises.ActionsWithCollections();
+
+//// for
+//var list = new List<int>();
+//for (int i = 0; i < 10; i += 2)
+//{
+//    list.Add(i);
+//}
+//for (int i = 0; i < list.Count; i++)
+//{
+//    Console.WriteLine(list[i]);
+//}
+
+
+////while
+//var list = new List<string>();
+//while (list.Count < 5)
+//{
+//    list.Add(Console.ReadLine());
+//}
+//int i = 0;
+//while (i < list.Count)
+//{
+//    Console.WriteLine(list[i]);
+//    i++;
+//}
+
+////do while
+//var list = new List<string>();
+//while (list.Count < 0)
+//{
+//    list.Add(Console.ReadLine());
+//}
+//var i = 0;
+//do
+//{
+
+//}
+//while (i < list.Count);
+
+//foreach
+//var list = new List<string>();
+//Random rnd = new Random();
+//for (int i = 0; i < 10; i++)
+//{
+//    list.Add(rnd.Next(0, 100).ToString());   
+//}
+//foreach (var item in list)
+//{
+//    Console.WriteLine(item);
+//}
+
---
+++ Lesson_0.IterationOperators/exercises.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Lesson_0.IterationOperators
+{
+    public class Exercises
+    {
+        public static void ActionsWithCollections()
+        {
+            List<int> list = new List<int>();
+            int sum = 0;           
+            int multiply = 1;
+            for (int i = 0; i < 15; i++)
+            {
+                Console.Write("Please, insert your number: \t");                
+                try
+                {
+                    int number = Convert.ToInt32(Console.ReadLine());
+                    list.Add(number);
+                }
+                catch(FormatException)
+                {
+                    Console.WriteLine("Input uncorrected. Please insert correct number: \t");                    
+                }
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                sum += list[i];
+            }
+
+            int j = 1;
+            int subtraction = list[0];
+            while (j < list.Count)
+            {
+                subtraction -= list[j];
+                j++;
+            }
+
+            int k = 0;
+            do
+            {
+                multiply *= list[k];
+                k++;
+            }
+            while (k < list.Count);
+
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine("\nThe sum is: \t\t" + sum);
+            Console.WriteLine("The subtraction is: \t" + subtraction);
+            Console.WriteLine("The multiply is: \t" + multiply);
+        }
+    }
+}
