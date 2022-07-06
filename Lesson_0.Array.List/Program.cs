3205c53092bfa8372bac55271bf2310270405ffd
אירינה קטלקין <ikatalkin@nitc.co.il>
2022-07-06 17:02:34 +0300

    Additions to lesson0.

---
+++ Lesson_0.Array.List/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] array = new int[10];
+array[0] = 1;
+array[1] = 2;
+array[2] = 3;
+array[9] = 5;
+
+Console.WriteLine($"{array[0]},{array[9]}");
+
+int[,] array1 = new int[5, 5];
+array1[0,0] = 6;
+array1[4,4] = 7;
+
+Console.WriteLine(array1[4,4]);
+
+List<int> list = new List<int>();
+list.Add(1);
+list.Add(2);
+list.Add(3);
+list.Add(4);
+
+List<int> list1 = new List<int>()
+{ 
+    1, 2, 3, 4, 5, 6, 7
+};
+
+list1.AddRange(list);
+
+Console.WriteLine(list1.Count);
+
+Console.WriteLine(Days.Wednesday);
+
+enum Days
+{
+    Sunday,
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday
+}
+
---
+++ Lesson_0.If.Switch/IsEvenBySwitch.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_0.If.Switch
+{
+    public class IsEvenBySwitch
+    {
+        public static void IsEven()
+        {
+            Console.WriteLine("Please, insert number:");
+            int number = Int32.Parse(Console.ReadLine());
+
+            switch (number % 2 == 0)
+            {
+                case true:
+                    Console.WriteLine("Your number is even");
+                    break;
+                case false:
+                    Console.WriteLine("Your number isn't even");
+                    break;
+
+            }
+        }
+    }
+}
