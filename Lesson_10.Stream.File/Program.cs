ed99c1a48acb0b9582368ae2ccf2d16b4796ea75
אירינה קטלקין <ikatalkin@nitc.co.il>
2022-07-10 23:08:25 +0300

    Add lesson10.Stream&File

---
+++ Lesson_10.Stream.File/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Lesson_10.Stream.File;
+using System.Text;
+
+////Console.OutputEncoding = Encoding.Unicode;
+//using (var sw = new StreamWriter("test.txt", true, Encoding.Unicode))
+//{
+    
+//    sw.WriteLine("Hello, World!!!");
+//    sw.WriteLine("Hello, User!!!");
+//    //sw.WriteLine("שלום");
+//    sw.WriteLine("Привет!!!");
+//}
+// using(var sr = new StreamReader("test.txt", Encoding.Unicode))
+//{
+//    while(!sr.EndOfStream)
+//    {
+//        Console.WriteLine(sr.ReadLine() + " End of line");
+//    }
+    
+//    sr.DiscardBufferedData();                    // clear the buffer
+//    sr.BaseStream.Seek(0, SeekOrigin.Begin);     //to start of file
+
+//    var text = sr.ReadToEnd();
+//    Console.WriteLine(text);
+//}
+
+WriteReadFile.WriteReadMyFile();
---
+++ Lesson_10.Stream.File/WriteReadFile.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_10.Stream.File
+{
+    public class WriteReadFile
+    {
+        //string name;
+        //string lastName;
+        //int age;
+        List<string> nameList = new List<string>();
+        List<string> lastNameList = new List<string>();
+        List<int> ageList = new List<int>();
+        public static void WriteReadMyFile()
+        {
+            Console.Write("Insert name:\t");
+            string name = Console.ReadLine();
+            //while (true)
+            //{
+            //    string input = Console.ReadLine();
+            //    if (!string.IsNullOrEmpty(input))
+            //    {                   
+            //        break;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("The name is incorrect. Please, insert again");
+            //    }
+            //}
+            //string name = input;
+            Console.Write("Insert lastName:\t");
+            //while (true)
+            //{
+            //    //string input = Console.ReadLine();
+            //    if (!string.IsNullOrEmpty(input))
+            //    {
+            //        break;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("The age is incorrect. Please, insert again");
+            //    }
+            //}
+            string lastName = Console.ReadLine();
+            Console.Write("Insert age:\t");
+            //while (true)
+            //{
+            //    string input = Console.ReadLine();
+            //    if (Int32.TryParse(input, out int result) && result > 0 && result < 120)
+            //    {
+            //        break;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("The age is incorrect. Please, insert again");
+            //    }
+            //}          
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Name is: {name}, Last Name is: {lastName}, Age is: {age}");
+
+            using (var sw = new StreamWriter("names.txt", true, Encoding.Unicode))
+            {
+                sw.WriteLine("Your name is: " +  lastName + " " + name +". And you are " + age);
+            }
+
+            using (var sr = new StreamReader("names.txt"))
+            {
+                var text = sr.ReadToEnd();
+                Console.WriteLine(text);
+            }
+        }
+        
+    }
+}
